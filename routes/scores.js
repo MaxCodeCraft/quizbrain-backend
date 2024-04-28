9aa4a30a923c1c4eb90c46b527ec2a02bf506bf7
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-04-28 19:24:19 +0200

    score route added

--- routes/scores.js
+++ routes/scores.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var router = express.Router();
+import Score from "../models/scores";
 
-router.get("/", (req, res) => {
-  // fetch('')
+router.get("/", async (req, res) => {
+  const response = await Score.find();
+  const data = await response.json();
+  res.json(data);
 });
 
 router.post("/new", (req, res) => {});
